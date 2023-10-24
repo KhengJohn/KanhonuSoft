fd67445a96e42ff1e46e7777205324453cda8a66
John Idoko <idokojohn42@yahoo.com>
2023-10-24 10:33:00 +0100

    Responsive Animation

--- js/animations.js
+++ js/animations.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function () {
   var controller = new ScrollMagic.Controller();
 
@@ -10,116 +9,104 @@ document.addEventListener("DOMContentLoaded", function () {
     .addTo(controller);
 });
 
-  
-    document.addEventListener("DOMContentLoaded", function () {
-      var controller = new ScrollMagic.Controller();
+document.addEventListener("DOMContentLoaded", function () {
+  var controller = new ScrollMagic.Controller();
 
-      var popUpScene = new ScrollMagic.Scene({
-        triggerElement: ".pop-up1",
-        triggerHook: 0.8,
-      })
-        .setClassToggle(".pop-up1", "active")
-        .addTo(controller);
-    });
+  var popUpScene = new ScrollMagic.Scene({
+    triggerElement: ".pop-up1",
+    triggerHook: 0.8,
+  })
+    .setClassToggle(".pop-up1", "active")
+    .addTo(controller);
+});
 
-       
-        document.addEventListener("DOMContentLoaded", function () {
-          var controller = new ScrollMagic.Controller();
-  
-          var popUpScene = new ScrollMagic.Scene({
-            triggerElement: ".pop-up2",
-            triggerHook: 0.9,
-          })
-            .setClassToggle(".pop-up2", "active")
-            .addTo(controller);
-        });
-      
-           
-            document.addEventListener("DOMContentLoaded", function () {
-              var controller = new ScrollMagic.Controller();
-      
-              var popUpScene = new ScrollMagic.Scene({
-                triggerElement: ".pop-up3",
-                triggerHook: 1.0,
-              })
-                .setClassToggle(".pop-up3", "active")
-                .addTo(controller);
-            });
-          
-           
-           
-             document.addEventListener("DOMContentLoaded", function () {
-               var controller = new ScrollMagic.Controller();
-       
-               var popUpScene = new ScrollMagic.Scene({
-                 triggerElement: ".pop-up4",
-                 triggerHook: 0.8,
-               })
-                 .setClassToggle(".pop-up4", "active")
-                 .addTo(controller);
-             });
-           
-                
-                 document.addEventListener("DOMContentLoaded", function () {
-                   var controller = new ScrollMagic.Controller();
-           
-                   var popUpScene = new ScrollMagic.Scene({
-                     triggerElement: ".pop-up5",
-                     triggerHook: 0.9,
-                   })
-                     .setClassToggle(".pop-up5", "active")
-                     .addTo(controller);
-                 });
-               
-                    
-                     document.addEventListener("DOMContentLoaded", function () {
-                       var controller = new ScrollMagic.Controller();
-               
-                       var popUpScene = new ScrollMagic.Scene({
-                         triggerElement: ".pop-up6",
-                         triggerHook: 1.0,
-                       })
-                         .setClassToggle(".pop-up6", "active")
-                         .addTo(controller);
-                     });
-                   
-                    
-                    
-                      document.addEventListener("DOMContentLoaded", function () {
-                        var controller = new ScrollMagic.Controller();
-                
-                        var popUpScene = new ScrollMagic.Scene({
-                          triggerElement: ".pop-up7",
-                          triggerHook: 0.8,
-                        })
-                          .setClassToggle(".pop-up7", "active")
-                          .addTo(controller);
-                      });
-                    
-                         
-                          document.addEventListener("DOMContentLoaded", function () {
-                            var controller = new ScrollMagic.Controller();
-                    
-                            var popUpScene = new ScrollMagic.Scene({
-                              triggerElement: ".pop-up8",
-                              triggerHook: 0.9,
-                            })
-                              .setClassToggle(".pop-up8", "active")
-                              .addTo(controller);
-                          });
-                        
-                             
-                              document.addEventListener("DOMContentLoaded", function () {
-                                var controller = new ScrollMagic.Controller();
-                        
-                                var popUpScene = new ScrollMagic.Scene({
-                                  triggerElement: ".pop-up9",
-                                  triggerHook: 1.0,
-                                })
-                                  .setClassToggle(".pop-up9", "active")
-                                  .addTo(controller);
-                              });
-                            
+document.addEventListener("DOMContentLoaded", function () {
+  var controller = new ScrollMagic.Controller();
+
+  var popUpScene = new ScrollMagic.Scene({
+    triggerElement: ".pop-up2",
+    triggerHook: 0.9,
+  })
+    .setClassToggle(".pop-up2", "active")
+    .addTo(controller);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var controller = new ScrollMagic.Controller();
+
+  var popUpScene = new ScrollMagic.Scene({
+    triggerElement: ".pop-up3",
+    triggerHook: 1.0,
+  })
+    .setClassToggle(".pop-up3", "active")
+    .addTo(controller);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var controller = new ScrollMagic.Controller();
+
+  var popUpScene = new ScrollMagic.Scene({
+    triggerElement: ".pop-up4",
+    triggerHook: 0.8,
+  })
+    .setClassToggle(".pop-up4", "active")
+    .addTo(controller);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var controller = new ScrollMagic.Controller();
+
+  var popUpScene = new ScrollMagic.Scene({
+    triggerElement: ".pop-up5",
+    triggerHook: 0.9,
+  })
+    .setClassToggle(".pop-up5", "active")
+    .addTo(controller);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var controller = new ScrollMagic.Controller();
+
+  var popUpScene = new ScrollMagic.Scene({
+    triggerElement: ".pop-up6",
+    triggerHook: 1.0,
+  })
+    .setClassToggle(".pop-up6", "active")
+    .addTo(controller);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var controller = new ScrollMagic.Controller();
+
+  var popUpScene = new ScrollMagic.Scene({
+    triggerElement: ".pop-up7",
+    triggerHook: 0.8,
+  })
+    .setClassToggle(".pop-up7", "active")
+    .addTo(controller);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var controller = new ScrollMagic.Controller();
+
+  var popUpScene = new ScrollMagic.Scene({
+    triggerElement: ".pop-up8",
+    triggerHook: 0.9,
+  })
+    .setClassToggle(".pop-up8", "active")
+    .addTo(controller);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var controller = new ScrollMagic.Controller();
+
+  var popUpScene = new ScrollMagic.Scene({
+    triggerElement: ".pop-up9",
+    triggerHook: 1.0,
+  })
+    .setClassToggle(".pop-up9", "active")
+    .addTo(controller);
+});
 
 document.addEventListener("DOMContentLoaded", function () {
   var controller = new ScrollMagic.Controller();
@@ -132,18 +119,16 @@ document.addEventListener("DOMContentLoaded", function () {
     .addTo(controller);
 });
 
-  
-    document.addEventListener("DOMContentLoaded", function () {
-      var controller = new ScrollMagic.Controller();
+document.addEventListener("DOMContentLoaded", function () {
+  var controller = new ScrollMagic.Controller();
 
-      var slideInSceneRight = new ScrollMagic.Scene({
-        triggerElement: ".slide-in-right2",
-        triggerHook: 0.8,
-      })
-        .setClassToggle(".slide-in-right2", "active")
-        .addTo(controller);
-    });
-  
+  var slideInSceneRight = new ScrollMagic.Scene({
+    triggerElement: ".slide-in-right2",
+    triggerHook: 0.8,
+  })
+    .setClassToggle(".slide-in-right2", "active")
+    .addTo(controller);
+});
 
 document.addEventListener("DOMContentLoaded", function () {
   var controller = new ScrollMagic.Controller();
